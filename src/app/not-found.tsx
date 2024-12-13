8d7f139cbcbcc1a6acfdf291bd5c1ed04ec03852
Luis Henrique <luishcc2003@gmail.com>
2024-12-12 21:06:18 -0300

    Added 'use client' to 'not-found' page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,7 @@
+"use client";
+
 import React from "react";
 
-export default function notFoundPage() {
-  return <div>404: Page not found</div>;
+export default function NotFoundPage() {
+  return <div className="p-2">404: Page not found</div>;
 }
